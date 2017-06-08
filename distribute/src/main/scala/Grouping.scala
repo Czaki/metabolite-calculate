c746b905b103c8848195280fd472ef532b58c2f4
Grzegorz Bokota <g.bokota@cent.uw.edu.pl>
2017-06-08 11:37:32 +0200

    add sumary of grouping

--- distribute/src/main/scala/Grouping.scala
+++ distribute/src/main/scala/Grouping.scala
@@ -1,13 +1,22 @@
 /**
   * Created by grzegorzbokota on 07.06.2017.
   */
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import java.io.{File, PrintWriter}
 
 
 object Grouping {
+  def getListOfSubDirectories(directoryName: String): Array[String] = {
+    new File(directoryName)
+      .listFiles
+      .filter(_.isDirectory)
+      .map(_.getName)
+  }
+
   def main(args: Array[String]) {
     if (args.length != 2){
       println(args(0).toString)
@@ -23,6 +32,17 @@ object Grouping {
     val key_data = data.map(v => (v.split("\\|")(1).trim, v))
     val df = key_data.toDF("target", "main")
     df.write.partitionBy("target").text(args(1))
+    val dirs = getListOfSubDirectories(args(1))
+    val summary_file = new PrintWriter(args(1), "summary.txt")
+    for (name <- dirs){
+      val path = new File(name, "part*").toString
+      val target_data = sc.textFile(path)
+      val size = target_data.count()
+      summary_file.write(name)
+      summary_file.write(": ")
+      summary_file.write(size.toString)
+    }
+    summary_file.close()
   }
 
 }
