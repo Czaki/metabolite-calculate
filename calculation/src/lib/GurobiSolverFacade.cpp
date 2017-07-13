8a55b7ab1f27f3851039405f9308fb943b40e8dd
Grzegorz Bokota <bokota@gmail.com>
2017-07-13 18:09:50 +0200

    Creating gurobi problem interface

--- calculation/src/lib/GurobiSolverFacade.cpp
+++ calculation/src/lib/GurobiSolverFacade.cpp
@@ -5,6 +5,7 @@
 #ifdef GUROBI
 #include "GurobiSolverFacade.h"
 #include <limits>
+#include <iostream>
 
 namespace PNFBA {
 GurobiSolverFacade::GurobiSolverFacade(
@@ -15,6 +16,8 @@ GurobiSolverFacade::GurobiSolverFacade(
     std::vector<int> &lp_system_i, std::vector<int> &lp_system_j,
     std::vector<double> &lp_system_val)
     : model(env) {
+  // col - reakcje
+  // row - metabolity
   std::unordered_map<std::string, GRBVar> variable_map;
   std::unordered_map<std::string, GRBLinExpr> equation_map;
   for(auto & name : lp_system_col_names){
@@ -23,8 +26,12 @@ GurobiSolverFacade::GurobiSolverFacade(
   for(auto & name : lp_system_row_names){
     equation_map[name] = 0.0;
   }
-  for (size_t i=0; i < lp_system_i.size(); i++){
-    equation_map[lp_system_row_names[lp_system_i[i]]] += lp_system_val[i] * variable_map[lp_system_col_names[lp_system_j[i]]];
+  for (size_t i=1; i < lp_system_i.size(); i++){
+    std::string name = lp_system_row_names[lp_system_i[i]-1];
+    if (name == "HC00178_c"){
+      std::cout << lp_system_val[i] << " " << lp_system_col_names[lp_system_j[i]-1] << std::endl;
+    }
+    equation_map[name] += lp_system_val[i] * variable_map[lp_system_col_names[lp_system_j[i]-1]];
   }
   for (auto & equation : equation_map){
     model.addConstr(equation.second == 0, equation.first);
