2d317a0c007b778a4cc35a93e1880e29b2db68b7
Grzegorz Bokota <g.bokota@cent.uw.edu.pl>
2018-05-31 13:37:56 +0200

    Upade to allow execute multiple ranges sequentially

--- distribute/distribute.sbt
+++ distribute/distribute.sbt
@@ -5,5 +5,6 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql"% "2.1.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql"% "2.3.0"
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.7.0"
--- distribute/src/main/scala/Distribution.scala
+++ distribute/src/main/scala/Distribution.scala
@@ -1,17 +1,41 @@
-/* SimpleApp.scala */
+/* Distribution.scala */
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+
 import scala.io.Source
 import java.io.File
 
 
+case class Config (
+                  executable: File = new File("metabolite_glpk"),
+                  range: (Int, Int) = (0, 0),
+                  metabolite_path: File = new File(""),
+                  output_path: File = new File("output"),
+                  parts_size: Int = 0,
+                  sfba_file: File = new File(""),
+                  qsspn_file: File =  new File(""),
+                  target: String = "",
+                  calculate_all: Boolean = true,
+                  from_file: Boolean = false,
+                  range_file: File = new File(""),
+                  single_step: Int = 100
+                  )
+
 
 object Distribution {
-  def getListOfFiles(dir: String):List[File] = {
+  def getListOfFiles(dir: String): List[File] = {
     val d = new File(dir)
     if (d.exists && d.isDirectory) {
       d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
+  def getListOfFiles(dir: File): List[File] = {
+    if (dir.exists && dir.isDirectory) {
+      dir.listFiles.filter(_.isFile).toList
     } else {
       List[File]()
     }
@@ -27,78 +51,123 @@ object Distribution {
     }
   }
 
-  def main(args: Array[String]) {
-    if (args.length < 4){
-      println(args(0).toString)
-      sys.error("Wrong execution arguments\n")
-      System.exit(1)
+  def get_parser(): scopt.OptionParser[Config] = {
+    new scopt.OptionParser[Config]("distribute metabolite") {
+      head("distribute metabolite", "1.0")
+      arg[File]("executable").required().action((p, c) => c.copy(executable = p))
+        .text("path to executable which calculate metabolite result")
+      arg[File]("metabolite_path").required().action((p, c) => {
+        val file_list = getListOfFiles(p)
+        if (file_list.isEmpty) {
+          System.err.println("[Error] No accces to qsspn and sfba file")
+          System.exit(-1)
+        }
+        val qsspn_file = file_list.filter(getFileExtension(_) == "qsspn").head
+        val sfba_file = file_list.filter(getFileExtension(_) == "sfba").head
+        c.copy(metabolite_path = p, qsspn_file = qsspn_file, sfba_file = sfba_file)
+      }).text("path to directory with qsspn and sfba files")
+      arg[File]("output_path").required().action((p, c) => c.copy(output_path = p)).
+        text("path to storage output")
+      arg[(Int, Int)]("range").optional().action((p, c) => c.copy(range = p, from_file = false, calculate_all = false))
+      opt[Int]('p', "parts_size").action((p, c) => c.copy(parts_size = p))
+      opt[File]("range_file").action((p, c) => c.copy(range_file = p, from_file = true, calculate_all = false))
+
+
+    }
+  }
+
+  def run_calculation(sc: SparkContext, config: Config, range: (Int, Int), destination: String): Unit = {
+    val distData = if (config.from_file) {
+      val pos = Source.fromFile(config.range_file).getLines().toArray.map(_.toInt)
+      val pos2 = pos.map(_ + 1)
+      val ranges = pos zip pos2
+      sc.makeRDD(ranges, 2000)
+    } else {
+      val ranges = {
+        val base_range = List.range(range._1, range._2, config.single_step)
+        if (range._2 % config.single_step != 0)
+          base_range ++ List(range._2)
+        else
+          base_range
+      }
+      sc.makeRDD(ranges zip ranges.tail, 2000)
     }
 
-    val executable = args(0)
-    val file_list = getListOfFiles(args(1))
-    if (file_list.isEmpty){
-      System.err.println("[Error] No accces to qsspn and sfba file")
-      System.exit(-1)
+    val c = {
+      distData.pipe(config.executable + " " + config.qsspn_file + " " + config.sfba_file + " --interactive" + {
+        if (config.target != "") {
+          "--target " + config.target
+        } else {
+          ""
+        }
+      })
     }
-    val qsspn_file = file_list.filter(getFileExtension(_) == "qsspn").head
-    val sfba_file = file_list.filter(getFileExtension(_) == "sfba").head
-    val conf = new SparkConf().setAppName("Simple Application") //.setMaster("local[4]")
-    val sc = new SparkContext(conf)
-    val lines = Source.fromFile(qsspn_file).getLines.toArray
+    c.saveAsTextFile(destination)
+  }
+
+  def calculate_number_of_combinations(config: Config): Int = {
+    val lines = Source.fromFile(config.qsspn_file).getLines.toArray
     var i = 0
     var combination_num = 1
 
-    while ( i < lines.length){
+    while (i < lines.length) {
       val s = lines(i).toString.trim
-      if (s.startsWith("ENZYME")){
+      if (s.startsWith("ENZYME")) {
         i += 1
         val s2 = lines(i).toString.trim
         combination_num *= s2.split(" ")(1).toInt
       }
       i += 1
     }
-    if (combination_num == 0){
+    if (combination_num == 0) {
       System.err.println("[Error] wrong combination num")
       System.exit(-1)
     }
-    println(combination_num)
-    val single_step = 100
-    val distData =  if (args.length == 4){
-      val pos = Source.fromFile(args(3)).getLines().toArray.map(_.toInt)
-      val pos2 = pos.map(_+1)
-      val ranges = pos zip pos2
-      sc.makeRDD(ranges, 2000)
-    } else {
-      if (args.length == 5) {
-        val ranges = {
-          val max_num = args(4).toInt
-          val base_range = List.range(args(3).toInt, max_num, single_step)
-          if (max_num % single_step != 0)
-            base_range ++ List(max_num)
-          else
-            base_range
-        }
-        //println(ranges zip ranges.tail)
-        sc.makeRDD(ranges zip ranges.tail, 2000)
-      } else {
-        val ranges = {
-          val base_range = List.range(0, combination_num, single_step)
-          if (combination_num % single_step != 0)
-            base_range ++ List(combination_num)
-          else
-            base_range
-        }
-        //println(ranges zip ranges.tail)
-        sc.makeRDD(ranges zip ranges.tail, 2000)
-      }
-    }
+    combination_num
+  }
 
-    val c = {
-      distData.pipe(executable + " " + qsspn_file + " " + sfba_file + " --interactive --target HC00178_b")
+  def main(args: Array[String]) {
+    println((11,6))
+    val parser = get_parser()
+    parser.parse(args, Config()) match {
+      case Some(config) => {
+        println(config)
+        sys.exit(1)
+        val conf = new SparkConf().setAppName("Distribute metabolite") //.setMaster("local[4]")
+        val sc = new SparkContext(conf)
+        if (config.from_file) {
+          run_calculation(sc, config, (0, 0), config.output_path.toString)
+          sc.stop()
+          sys.exit(0)
+        }
+        val range = if (config.calculate_all) {
+          (0, calculate_number_of_combinations(config))
+        } else {
+          config.range
+        }
+        if (config.parts_size > 0) {
+          val ranges = {
+            val base_range = List.range(range._1, range._2, config.parts_size)
+            if (range._2 % config.parts_size != 0)
+              base_range ++ List(range._2)
+            else
+              base_range
+          }
+          for ((part_range, i) <- (ranges zip ranges.tail).zipWithIndex) {
+            run_calculation(sc, config, part_range, new File(config.output_path, "part" + i.toString).toString)
+
+          }
+        } else {
+          run_calculation(sc, config, range, config.output_path.toString)
+        }
+        sc.stop()
+      }
+
+      case None => {
+        sys.exit(-1)
+      }
+
+
     }
-    //print("Res: ")
-    //println(c.count())
-    c.saveAsTextFile(args(2))
-    sc.stop()
   }
 }
